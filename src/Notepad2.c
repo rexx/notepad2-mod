3bbf095bb28ea94487a606eae092c1bbd9fdad86
XhmikosR <xhmikosr@users.sourceforge.net>
2013-02-01 12:51:30 +0200

    Add support for Matlab

--- src/Notepad2.c
+++ src/Notepad2.c
@@ -3434,6 +3434,7 @@ LRESULT MsgCommand(HWND hwnd,WPARAM wParam,LPARAM lParam)
           EditToggleLineComments(hwndEdit,L"rem ",TRUE);
           EndWaitCursor();
           break;
+        case SCLEX_MATLAB:
         case SCLEX_LATEX:
           BeginWaitCursor();
           EditToggleLineComments(hwndEdit,L"%",TRUE);
@@ -3482,6 +3483,9 @@ LRESULT MsgCommand(HWND hwnd,WPARAM wParam,LPARAM lParam)
           break;
         case SCLEX_LUA:
           EditEncloseSelection(hwndEdit,L"--[[",L"]]");
+          break;
+        case SCLEX_MATLAB:
+          EditEncloseSelection(hwndEdit,L"%{",L"%}");
       }
       break;
 
@@ -6235,7 +6239,7 @@ void ParseCommandLine()
             LocalFree(lpSchemeArg);
             lpSchemeArg = NULL;
           }
-          iInitialLexer = 33;
+          iInitialLexer = 34;
           flagLexerSpecified = 1;
           break;
 
@@ -6244,7 +6248,7 @@ void ParseCommandLine()
             LocalFree(lpSchemeArg);
             lpSchemeArg = NULL;
           }
-          iInitialLexer = 34;
+          iInitialLexer = 35;
           flagLexerSpecified = 1;
           break;
 
